9cbd9c83c16e1f52e1e6a972baa013f1902d743a
YanChongWen <yanchongwen101@163.com>
2020-12-02 22:26:13 +0800

    ① 先暂时关闭【SVG】文件的体积精简，以实现初步的成功打包。

--- package-scripts.js
+++ package-scripts.js
@@ -3,7 +3,7 @@ const npsUtils = require('nps-utils'); // not required, but handy!
 module.exports = {
   scripts: {
     // 'dev': 'parcel ./index.html ',
-    'predev': npsUtils.series(
+    'pre_dev_build': npsUtils.series(
       `echo  "开始predev"  `,
       // `( mkdir  dist || exit 0 )`,  // 只是为了创建一个目录
       `( mkdir  dist || echo '目录已存在' )`,  // 只是为了创建一个目录
@@ -12,10 +12,14 @@ module.exports = {
       `ncp    ./apijson     ./dist/apijson    --filter '**/*.*'`,
       `ncp    ./md          ./dist/md         --filter '**/*.*'`,
     ),
-    'dev'   : npsUtils.series(
+    'dev'          : npsUtils.series(
       `echo "开始dev"  `,
       'parcel ./index.html',
     ),
-    'build' : 'parcel build ./index.html js/**/*',
+    /**
+     * 1.临时先关闭【SVG】的体积精简，以避免触发报错。
+     *        1.留待以后有时间时，再加以解决。
+     */
+    'build'        : 'parcel build ./index.html --no-minify',
   }
 };
