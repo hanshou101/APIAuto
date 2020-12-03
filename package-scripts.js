a28dc8a941645ca211accfed9f1b8c5b061502f5
YanChongWen <yanchongwen101@163.com>
2020-12-03 10:24:39 +0800

    ① 已经修正了【打包】的问题。现在可以全程正常打包。

--- package-scripts.js
+++ package-scripts.js
@@ -8,9 +8,11 @@ module.exports = {
       // `( mkdir  dist || exit 0 )`,  // 只是为了创建一个目录
       `( mkdir  dist || echo '目录已存在' )`,  // 只是为了创建一个目录
       //
+      //
       `ncp    ./js          ./dist/js         --filter '**/*.*'`,
       `ncp    ./apijson     ./dist/apijson    --filter '**/*.*'`,
       `ncp    ./md          ./dist/md         --filter '**/*.*'`,
+      `ncp    ./svg         ./dist/svg        --filter '**/*.*'`,   // WARN 为了处理【--no-minify】后，svg相关文件，打包后位置不正确的问题
     ),
     'dev'          : npsUtils.series(
       `echo "开始dev"  `,
@@ -20,6 +22,6 @@ module.exports = {
      * 1.临时先关闭【SVG】的体积精简，以避免触发报错。
      *        1.留待以后有时间时，再加以解决。
      */
-    'build'        : 'parcel build ./index.html --no-minify',
+    'build'        : 'parcel build ./index.html    --no-minify     --public-url ./    ',
   }
 };
